9da5873ec8ee47db241aabad0264baa4c540a123
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-10-13 18:30:37 +0300

    Add more information about script's work in task 2 of the lection 1

--- Lection 1/Task 2/main.py
+++ Lection 1/Task 2/main.py
@@ -1,3 +1,4 @@
+print("Let's calculate the sum of two numbers. You need to enter two numbers that you want to sum up.")
 first_number = int(input("Enter the first number: "))
 second_number = int(input("Enter the second number: "))
 
