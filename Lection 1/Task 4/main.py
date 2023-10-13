a2ee59dadbbcbe61138abe9b82194a7c5b0ed033
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-10-13 18:39:40 +0300

    Refactor code (remove copypasta) in task 4 of the first lection

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,29 +1,29 @@
 import math
 
 
-def request_number(text):
+def request_input_in_float(text):
     return float(input(text))
 
 
 def calculate(action, first_number):
+    if action in ["+", "-", "*", "/"]:
+        second_number = request_input_in_float("Enter the second number: ")
+    elif action == "exponent":
+        second_number = request_input_in_float("Enter exponent: ")
+
     match action:
         case "+":
-            second_number = request_number("Enter the second number: ")
             result = first_number + second_number
         case "-":
-            second_number = request_number("Enter the second number: ")
             result = first_number - second_number
         case "*":
-            second_number = request_number("Enter the second number: ")
             result = first_number * second_number
         case "/":
-            second_number = request_number("Enter the second number: ")
             result = first_number / second_number
         case "root":
             result = math.sqrt(first_number)
         case "exponent":
-            exponent = request_number("Enter exponent: ")
-            result = first_number ** exponent
+            result = first_number ** second_number
         case _:
             print("Error, you have selected an unknown action ._.")
     return result
@@ -31,5 +31,5 @@ def calculate(action, first_number):
 
 if __name__ == "__main__":
     action = input("Select the required action (+, -, *, /, root, exponent): ")
-    first_number = request_number("Enter the first number: ")
+    first_number = request_input_in_float("Enter the first number: ")
     print(f"Result: {calculate(action, first_number)}")
