122d54efd853ba01424b83549bfa8705a0758598
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-10-13 21:41:57 +0300

    Upload first task of the second lection

---
+++ Lection 2/Task 1/main.py
@@ -0,0 +1,11 @@
+print("Enter any item (number, word, etc.). To finish entering items, enter \"stop this list\" as the item.")
+
+arr = []
+while True:
+    element = input("Enter an element: ")
+    if element == "stop this list":
+        break
+
+    arr.append(element)
+
+print(f"Your list of items (only unique items will be displayed): {set(arr)}")
