9cfac162a8ea2c4617a5d0b2ea10ec7378f0d98e
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-11-09 23:25:08 +0200

    Replace error handler with another one way of recognizing number in the second task of the second lection

--- Lection 2/Task 2/main.py
+++ Lection 2/Task 2/main.py
@@ -1,11 +1,21 @@
+digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 elements = input("Enter any items (enter with a space): ").split()
 
 numbers_list = []
 for element in elements:
-    try:
-        number = int(element)
-        numbers_list.append(number)
-    except ValueError:
-        continue
+    is_number = True
+
+    for index, symbol in enumerate(element):
+        if symbol not in digits:
+            if index == 0:
+                if symbol == "+" or symbol == "-":
+                    continue
+            
+            is_number = False
+            break
+    
+    if is_number:
+        numbers_list.append(element)
 
 print(f"You've entered {len(numbers_list)} numbers. List of the numbers you have entered: {numbers_list}")
