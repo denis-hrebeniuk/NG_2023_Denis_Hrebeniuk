7039f6340236e02133360b9d514b358d06fa4698
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-11-09 23:24:47 +0200

    Change calculate() function to use operator lib, optimize it and rewrite a call of this function (the fourth task of the first lection)

--- Lection 1/Task 4/main.py
+++ Lection 1/Task 4/main.py
@@ -1,4 +1,11 @@
-import math
+import operator
+
+operators = {
+    "+": operator.add,
+    "-": operator.sub,
+    "*": operator.mul,
+    "/": operator.truediv,
+}
 
 
 def request_input_in_float(text):
@@ -6,30 +13,30 @@ def request_input_in_float(text):
 
 
 def calculate(action, first_number):
-    if action in ["+", "-", "*", "/"]:
-        second_number = request_input_in_float("Enter the second number: ")
-    elif action == "exponent":
-        second_number = request_input_in_float("Enter exponent: ")
+    result = None
 
     match action:
-        case "+":
-            result = first_number + second_number
-        case "-":
-            result = first_number - second_number
-        case "*":
-            result = first_number * second_number
-        case "/":
-            result = first_number / second_number
+        case "+" | "-" | "*" | "/":
+            second_number = request_input_in_float("Enter the second number: ")
+            result = operators[action](first_number, second_number)
         case "root":
-            result = math.sqrt(first_number)
+            exponent = request_input_in_float("Enter exponent of the root: ")
+            result = first_number ** (1 / exponent)
         case "exponent":
-            result = first_number ** second_number
+            exponent = request_input_in_float("Enter exponent: ")
+            result = first_number ** exponent
         case _:
-            print("Error, you have selected an unknown action ._.")
+            result = None
+    
     return result
 
 
 if __name__ == "__main__":
     action = input("Select the required action (+, -, *, /, root, exponent): ")
-    first_number = request_input_in_float("Enter the first number: ")
-    print(f"Result: {calculate(action, first_number)}")
+    first_number = request_input_in_float("Enter the number: ")
+
+    result = calculate(action, first_number)
+    if result:
+        print(f"Result: {result}")
+    else:
+        print("Error, you have selected an unknown action ._.")
