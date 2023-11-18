461ffad468a18f0fabf93af9d7800d61314defc4
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-11-18 21:39:46 +0200

    Simplified loops, replaced while with for, rewrote the search for prime numbers in the third task of the second lection

--- Lection 2/Task 3/main.py
+++ Lection 2/Task 3/main.py
@@ -6,25 +6,22 @@ if final_number < 0:
 # First subtask
 print("=" * 64)
 print("Number\tNumbers, by which a number is divisible without remainder")
-number = 1
-while number < final_number:
+for number in range(1, final_number):
     multiples = []
-    divisible = number
-    while divisible > 0:
-        if number % divisible == 0:
-            multiples.append(divisible)
-        
-        divisible -= 1
+    for divider in range(1, number + 1):
+        if number % divider == 0:
+            multiples.append(divider)
     print(f"{number}\t{multiples}")
-    number += 1
+    
 
-# Second subtask
+# Second subtask (https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes)
 print("=" * 64)
 print("Nice! Here's a list of all the prime numbers up to the number you entered:")
-number = 1
-prime_numbers = []
-while number < final_number:
-    if number % 2 != 0:
-        prime_numbers.append(number)
-    number += 1
-print(prime_numbers)
+natural_numbers = list(range(2, int(final_number)))
+for number in range(2, final_number):
+    if number in natural_numbers:
+        if number * number < final_number:
+            for natural_number in natural_numbers.copy():
+                if natural_number != number and natural_number % number == 0:
+                    natural_numbers.remove(natural_number)
+print(natural_numbers)
