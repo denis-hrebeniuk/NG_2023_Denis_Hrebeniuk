4000a97f2f940a0e50555c8ca969a2c86f5a5f9d
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-11-23 12:28:19 +0200

    Use isdigit() instead loop-way (digits list) in second task of the second lection

--- Lection 2/Task 2/main.py
+++ Lection 2/Task 2/main.py
@@ -4,18 +4,7 @@ elements = input("Enter any items (enter with a space): ").split()
 
 numbers_list = []
 for element in elements:
-    is_number = True
-
-    for index, symbol in enumerate(element):
-        if symbol not in digits:
-            if index == 0:
-                if symbol == "+" or symbol == "-":
-                    continue
-            
-            is_number = False
-            break
-    
-    if is_number:
+    if element.isdigit():
         numbers_list.append(element)
 
 print(f"You've entered {len(numbers_list)} numbers. List of the numbers you have entered: {numbers_list}")
