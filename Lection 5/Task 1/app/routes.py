3135608a080471d7ee62b89d3dc320d89d887ee9
Denis Hrebeniuk <denis.hrebeniuk2006@gmail.com>
2023-11-30 11:56:22 +0200

    Add a frontend to work with backend of the web application, rewrite routes to handle POST/GET requests

--- Lection 5/Task 1/app/routes.py
+++ Lection 5/Task 1/app/routes.py
@@ -1,28 +1,41 @@
 from app import app, db
 from app.models import Users, Messages
-from flask import request, redirect
+from flask import request, redirect, render_template
 from markupsafe import Markup
+
+
+def send_html_error(message, redirect_path):
+    message += f'<br /><br /><a href="{redirect_path}"><button>Go back</button></a>'
+    return Markup(message)
 
 
 @app.route("/")
 def index():
-    return "Hello, app is working!"
+    return render_template("index.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
-    login = request.args.get("login")
-    password = request.args.get("password")
-    password_confirm = request.args.get("password_confirm")
+    if request.method == "GET":
+        return render_template("register.html")
+
+    login = request.form.get("login")
+    password = request.form.get("password")
+    password_confirm = request.form.get("password_confirm")
 
     if not login or not password or not password_confirm:
-        return "Please, pass all expected arguments (login, password and password confirmation)"
+        return send_html_error(
+            "Please, pass all expected arguments (login, password and password confirmation)",
+            "register",
+        )
 
     if password_confirm != password:
-        return "Password and password confirmation do not match"
+        return send_html_error(
+            "Password and password confirmation do not match", "register"
+        )
 
     if Users.query.where(Users.login == login).all():
-        return "The login you passed is already taken"
+        return send_html_error("The login you passed is already taken", "register")
 
     db.session.add(Users(login=login, password=password))
     db.session.commit()
@@ -30,14 +43,19 @@ def register():
     return redirect("/all", code=302)
 
 
-@app.route("/send")
+@app.route("/send", methods=["GET", "POST"])
 def send():
-    login = request.args.get("login")
-    password = request.args.get("password")
-    message = request.args.get("message")
+    if request.method == "GET":
+        return render_template("send.html")
+
+    login = request.form.get("login")
+    password = request.form.get("password")
+    message = request.form.get("message")
 
     if not login or not password or not message:
-        return "Please, pass all expected arguments (login, password, message)"
+        return send_html_error(
+            "Please, pass all expected arguments (login, password, message)", "send"
+        )
 
     response = Users.query.where(Users.login == login).all()
     if response and response[0].password == password:
@@ -45,7 +63,7 @@ def send():
         db.session.commit()
         return redirect("/all", code=302)
 
-    return "Invalid login or password"
+    return send_html_error("Invalid login or password", "send")
 
 
 @app.route("/all")
@@ -55,10 +73,10 @@ def all():
     for message in Messages.query.all():
         user = Users.query.filter(Users.id == message.user_id).all()
         data += (
-            f"<b>{user[0].login} [{message.message_date}]:</b> {message.message}<br/>"
+            f"<b>{user[0].login} [{message.message_date}]:</b> {message.message}<br />"
         )
 
     if not data:
         data = "Messages not found"
 
-    return Markup(data)
+    return render_template("all.html", data=Markup(data))
